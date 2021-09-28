5d8bac233498539a61e57eb74650326bf9f3b1fd
jamesonjackson <jjackson@newvisions.org>
2021-09-27 22:36:34 -0400

    Clean up postheader and link and conditionally show article description

--- components/molecules/PostHeader/PostHeader.js
+++ components/molecules/PostHeader/PostHeader.js
@@ -1,24 +1,32 @@
-export const PostHeader = ({ meta, isBlogPost }) => (
-  <>
-    <h1 className={isBlogPost ? 'great-title' : null}>
-      {meta.title}
-    </h1>
+import styles from './PostHeader.module.scss';
 
-    <div className='details'>
-      { 
-        isBlogPost ? 
-        null : 
-        <p>{meta.description}</p>
-      }
-      
-      <span>{meta.date}</span>
-      
-      <span 
-        role='img' 
-        aria-label='one coffee'
-      >
-        ☕ {`${meta.readTime}min read`}
-      </span>
+export const PostHeader = ({ meta, isBlogPost }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        {meta.title}
+      </h1>
+
+      <div className={styles.details}>
+        {
+          isBlogPost || !meta.description ?
+          null :
+          <p className={styles.description}>{meta.description}</p>
+        }
+
+        <span>{meta.date}</span>
+
+        <span>&#8212;</span>
+
+        <span
+          role='img'
+          aria-label='one coffee'
+          className=""
+        >
+          ☕ {`${meta.readTime}min read`}
+        </span>
+      </div>
     </div>
-  </>
-)
+  )
+}
+
--- components/organisms/Post/Post.js
+++ components/organisms/Post/Post.js
@@ -1,9 +1,9 @@
 import { PostHeader } from '../../molecules/PostHeader/PostHeader'
 
-export default function Post({ children, meta}) {
+export default function Post({children, meta}) {
   return (
     <>
-      <PostHeader meta={meta} isBlogPosts/>
+      <PostHeader meta={meta} isBlogPost={true}/>
       
       <article>{children}</article>
     </>
