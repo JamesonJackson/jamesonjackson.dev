cec5837a4fdf950d9e203ef3efa191cb99e11aa0
JJ <jamesonjackson.code@gmail.com>
2022-02-22 06:53:42 -0500

    format codeblock

--- components/atoms/CodeBlock/CodeBlock.js
+++ components/atoms/CodeBlock/CodeBlock.js
@@ -1,10 +1,11 @@
 import Highlight, {defaultProps} from 'prism-react-renderer'
+import styles from './CodeBlock.module.scss'
 
 export function CodeBlock ({children}) {
   return (
     <Highlight {...defaultProps} code={children} language="javascript">
       {({className, style, tokens, getLineProps, getTokenProps}) => (
-        <pre className={className} style={{...style, padding: '20px'}}>
+        <pre className={styles.pre} style={{...style, padding: '20px'}}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({line, key: i})}>
               {line.map((token, key) => (
